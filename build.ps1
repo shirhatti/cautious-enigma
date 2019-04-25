d412bdb143d15a26c4ed9092ad035aff5b5c58b2
Sourabh Shirhatti <shirhatti@outlook.com>
2019-04-25 01:22:05 -0700

    Wire up oras.exe (win only)

--- build.ps1
+++ build.ps1
@@ -1,2 +1,4 @@
 dotnet pack .\src\Cautious.Enigma\
+rm -r .\samples\ConsoleApp\obj
 dotnet restore .\samples\ConsoleApp\
+dotnet publish .\samples\ConsoleApp
--- src/Cautious.Enigma/OrasPushTask.cs
+++ src/Cautious.Enigma/OrasPushTask.cs
@@ -1,12 +1,25 @@
 using Microsoft.Build.Framework;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 
 namespace MSBuildTasks
 {
     public class OrasPush : Microsoft.Build.Utilities.Task
     {
+        [Required]
+        public string OrasExe { get; set; }
+
         public override bool Execute()
         {
-            Log.LogMessage(MessageImportance.High, "Aloha");
+            var psi = new ProcessStartInfo(fileName: OrasExe,
+                                           arguments: "--help");
+            psi.RedirectStandardOutput = true;
+            using (var proc = Process.Start(psi))
+            {
+                proc.WaitForExit();
+                Log.LogMessage(MessageImportance.High, proc.StandardOutput.ReadToEnd());
+            }
             return true;
         }
     }
